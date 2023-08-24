2adb6a9e45078fec2ee0a93eff22fb4596b13205
SalsabilaZaman <140091958+SalsabilaZaman@users.noreply.github.com>
2023-08-25 01:04:31 +0600

    Add files via upload

---
+++ primeFactorization.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+#define MAX 1000007
+int smallestPrimeFactor[MAX];
+
+void smallestPrimeFinder(){
+	int i,j;
+	for(i=1;i<MAX;i++)
+	   smallestPrimeFactor[i]=i;
+	for(i=4;i<MAX;i+=2)
+	   smallestPrimeFactor[i]=2;
+	for(i=3;i*i<MAX;i++)
+	   if(smallestPrimeFactor[i]==i)
+	      for(j=i*i;j<MAX;j+=i)
+	          smallestPrimeFactor[j]=i;  	  
+
+}
+
+void primeFactorization(int num){
+	smallestPrimeFinder();
+	int i;
+	cout <<"Prime Factorization of "<< num <<"-" << endl;
+	for(i=num;i>1;){
+		cout << smallestPrimeFactor[i];
+		i=i/smallestPrimeFactor[i];
+		if(i!=1)
+		  cout <<" X ";
+	}
+}
+int main(){
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+	
+	primeFactorization(num);
+	
+	return 0;
+}
