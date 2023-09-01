a27f6a6e956542918e84597172921388d3e15403
SalsabilaZaman <140091958+SalsabilaZaman@users.noreply.github.com>
2023-09-01 08:44:38 +0600

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+
+
+void GCDcalculate(){
+		int n;
+		cout << "How many Number-";
+		cin >> n;
+		int arr[n];
+		for(int i=0;i<n;i++){
+		    cout << "Enter Number-" << i+1 << ":";
+		    cin >> arr[i];
+		}
+		int swap;         
+		int big=arr[0];     
+		 for(int i=1;i<n;i++){
+			int remainder=-1;
+			int small=arr[i];
+		      if(big<small){
+		      	swap=big;
+			big=small;		      
+		        small=swap;
+		      }
+		     while(remainder!=0){
+			 remainder=big%small;
+			 big=small;
+			 small=remainder;
+		     } 
+		     if (big==1)
+		        break;        
+		 }      
+		cout << "GCD of given "<< n <<" numbers is-"<< big << endl;
+}
+
+int main(){
+	GCDcalculate();
+	return 0;
+}
