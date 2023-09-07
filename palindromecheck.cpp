a0d694dc75087271305797683995453af81f085d
SalsabilaZaman <140091958+SalsabilaZaman@users.noreply.github.com>
2023-09-07 09:44:36 +0600

    palindrome

---
+++ palindromecheck.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void palindromeCheck(){
+	string str;
+	cout <<"Enter number:";
+	cin >> str;
+	int len=str.size();
+	bool result=true;
+	for(int i=0;i<=len/2;i++)
+		if(str[i]!=str[len-1-i]){
+			result=false;
+			break;
+		}
+		      
+	if(result==true)
+	  cout <<"\nPalindrome!\n";
+	else
+	  cout <<"\nNot Palindrome!\n";  	
+
+}
+
+int main(){
+	palindromeCheck();
+	return 0;
+}
