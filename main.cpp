bb9de1afea1a473422301c80f88c012c7d7b57c2
SalsabilaZaman <140091958+SalsabilaZaman@users.noreply.github.com>
2023-09-07 09:56:47 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include "digits.cpp"
+#include "factorial.cpp"
+#include "fibonacci.cpp"
+#include "gcd.cpp"
+#include "lcm.cpp"
+#include "matrixAddition.cpp"
+#include "palindromecheck.cpp"
+#include "primeFactorization.cpp"
+#include "primeNumberCheck.cpp"
+
+
+int main(){
+	cout << "Operations you can do-\n";
+	cout << "1.Prime Checker\n";
+	cout << "2.Prime Generator(in a given range)\n";
+	cout << "3.Prime Factorization\n";
+	cout << "4.Digit Summation\n";
+	cout << "5.Palindrome Checker\n";
+	cout << "6.GCD Calculator\n";
+	cout << "7.LCM Calculator\n";
+	cout << "8.Fibonacci Sequence Generator\n";
+	cout << "9.Matrix Addition\n";
+	
+	
+}
